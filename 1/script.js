18de81bb86e232ac27526abe016351d76f7099ce
Arasu Ramanan <arasuaudi@gmail.com>
2026-01-21 16:08:42 +0530

    Second commit

--- 1/script.js
+++ 1/script.js
@@ -2,7 +2,9 @@ async function quotes(){
  let response = await fetch("https://dummyjson.com/quotes")
     let result =   await response.json()
     console.log(result.quotes);
-    document.writeln(result.quotes[10].quote)
+    document.writeln("Quote: " + result.quotes[1].quote + "<br>")
+    document.writeln("Author: " + result.quotes[1].author)
+
 
 }
 quotes()
